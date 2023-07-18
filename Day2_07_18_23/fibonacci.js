9d66aafd6f992d4303e948bc7653fb640abd49ce
Haritha <136883345+HarithaMV18@users.noreply.github.com>
2023-07-18 20:50:00 +0530

    Update fibonacci.js

--- Day2_07_18_23/fibonacci.js
+++ Day2_07_18_23/fibonacci.js
@@ -1,17 +1,28 @@
+function fibonacciSeries(num){
 let f1=0;
 let f2=1;
 let f3=0;
 let fibonacciArr=[];
-function fibonacciSeries(num){
     for(let i=0;i<num;i++){
-        fibonacciArr.push(f3);
-        f3=f1+f2;
+        if(i===0){
+          fibonacciArr.push(f1);
+        }
+        else if(i===1){
+          fibonacciArr.push(f2);
+        }
+       else{
+           f3=f1+f2
         f1=f2;
         f2=f3
+        fibonacciArr.push(f3);
+
+       }
+     
+
         
         
     }
-    
+    return fibonacciArr;
 }
-fibonacciSeries(10);
-console.log(fibonacciArr)
+let c=fibonacciSeries(10);
+console.log(c)
